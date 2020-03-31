ebfe73bb65585aefbf05fa05a8cb174de97789e8
Claire <ptixibit@hotmail.fr>
2020-03-31 10:57:24 +0200

    test

--- Routes/offer.js
+++ Routes/offer.js
@@ -19,10 +19,11 @@ const isAuthenticated = require("../Middleware/isAuthenticated");
 router.post("/offer/publish", isAuthenticated, async (req, res) => {
   try {
     const files = Object.keys(req.files);
-    console.log(files);
+    console.log(req.files);
     if (files.length) {
       const pictures = [];
       files.forEach(key => {
+        console.log(key);
         cloudinary.uploader.upload(
           req.files[key].path,
           {
