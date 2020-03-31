302ff8ac922d04cc609576feced44814532eef1d
Claire <ptixibit@hotmail.fr>
2020-03-31 17:09:44 +0200

    test

--- Routes/offer.js
+++ Routes/offer.js
@@ -20,12 +20,11 @@ router.post("/offer/publish", isAuthenticated, async (req, res) => {
   try {
     const files = Object.keys(req.files);
     console.log(1);
-    console.log("req.files===> ", req.fields.files);
-    console.log("length===> ", req.fields.files.length);
     if (files.length) {
       const pictures = [];
       console.log(2);
       files.forEach(key => {
+        console.log("key==> ", key);
         console.log(3);
         cloudinary.uploader.upload(
           req.files[key][0].path,
